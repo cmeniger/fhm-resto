8f79837f459b52069c53144136727984523da3d7
reap <reap@fhmsolutions.com>
2016-11-10 16:03:00 +0100

    Ajout des routes pour l'Oauth

--- src/Core/UserBundle/Controller/DefaultController.php
+++ src/Core/UserBundle/Controller/DefaultController.php
@@ -4,10 +4,50 @@ namespace Core\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * @Route
+ * (
+ *      path="/user",
+ * )
+ */
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('CoreUserBundle:Default:index.html.twig');
+    }
+
+
+    /**
+     * @Route
+     * (
+     *      path="/check/facebook",
+     *      name="fhm_user_check_facebook"
+     * )
+     */
+    public function checkFacebookAction()
+    {
+    }
+
+    /**
+     * @Route
+     * (
+     *      path="/check/twitter",
+     *      name="fhm_user_check_twitter"
+     * )
+     */
+    public function checkTwitterAction()
+    {
+    }
+
+    /**
+     * @Route
+     * (
+     *      path="/check/google",
+     *      name="fhm_user_check_google"
+     * )
+     */
+    public function checkGoogleAction()
+    {
     }
 }
