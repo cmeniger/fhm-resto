b0f01e387f46bb5a39ecc80f633f9580a9b2e3d3
asy <abdoulaye2015>
2016-11-17 19:56:42 +0100

    glo

--- src/Core/CodeExplorerBundle/EventListener/GlobalDataEventListener.php
+++ src/Core/CodeExplorerBundle/EventListener/GlobalDataEventListener.php
@@ -6,10 +6,40 @@
  * Time: 17:27
  */
 
-namespace Core\CodeExplorerBundle\EventListener;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
-
-class GlobalDataEventListener
+class GlobalDataEventListener implements EventSubscriberInterface
 {
+    private $twig;
 
+    public function __construct($twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(KernelEvents::CONTROLLER => array(
+            array('sendTwig', 0),
+        ));
+    }
+    public function sendTwig(FilterControllerEvent $event)
+    {
+        $controller = $event->getController();
+        $request = $event->getRequest();
+        $attributes = $request->attributes->all();
+        // Ignore sub requests
+        if (HttpKernel::MASTER_REQUEST != $event->getRequestType()) {
+
+            return;
+        }
+//        $instanceData = array();
+//        if (method_exists($controller, 'getControllerInfo')) {
+//            $instanceData = $controller->getControllerInfo();
+//        }
+
+//        $instanceData = array_merge($instanceData,$attributes);
+
+        $this->twig->addGlobal('toto', $attributes);
+    }
 }
