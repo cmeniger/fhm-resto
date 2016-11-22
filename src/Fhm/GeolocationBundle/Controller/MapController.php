d7ec4bdeac98d16cfdca146065998f0afc1e88c9
reap <reap@fhmsolutions.com>
2016-11-22 15:31:20 +0100

    Ajout du service fhm_tools Geolocation

--- src/Fhm/GeolocationBundle/Controller/MapController.php
+++ src/Fhm/GeolocationBundle/Controller/MapController.php
@@ -5,10 +5,17 @@ namespace Fhm\GeolocationBundle\Controller;
 use Fhm\FhmBundle\Controller\FhmController as FhmController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
  * @Route("/map")
  */
-class MapController extends FhmController
+class MapController extends Controller
 {
+    protected $fhm_tools;
+
+    public function __construct($fhm_tools)
+    {
+        $this->fhm_tools = $fhm_tools;
+    }
 }
