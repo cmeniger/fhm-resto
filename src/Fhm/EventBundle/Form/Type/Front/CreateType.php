cd6b49ca5208f5e0e2daec85466f43696fd5138e
reap <reap@fhmsolutions.com>
2016-11-25 14:43:05 +0100

    Correction form Bundle event gallery

--- src/Fhm/EventBundle/Form/Type/Front/CreateType.php
+++ src/Fhm/EventBundle/Form/Type/Front/CreateType.php
@@ -8,6 +8,7 @@ class CreateType extends FhmType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $this->setTranslation('event');
         parent::buildForm($builder, $options);
     }
 }
