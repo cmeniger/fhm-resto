fc9629e2ba4823caa7614e6d0943042cf71845f6
reap <reap@fhmsolutions.com>
2017-01-17 11:03:54 +0100

    Entity Article

---
+++ src/Fhm/ArticleBundle/Entity/Article.php
@@ -0,0 +1,167 @@
+<?php
+namespace Fhm\ArticleBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Fhm\FhmBundle\Document\Fhm;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table()
+ */
+class Article extends Fhm
+{
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $title;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $subtitle;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $resume;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $content;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Media")
+     */
+    protected $image;
+
+    /**
+     * @ORM\OneToOne(targetEntity="Gallery")
+     */
+    protected $gallery;
+
+    /**
+     * @ORM\OneToOne(targetEntity="User")
+     */
+    protected $author;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubtitle()
+    {
+        return $this->subtitle;
+    }
+
+    /**
+     * @param mixed $subtitle
+     */
+    public function setSubtitle($subtitle)
+    {
+        $this->subtitle = $subtitle;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getResume()
+    {
+        return $this->resume;
+    }
+
+    /**
+     * @param mixed $resume
+     */
+    public function setResume($resume)
+    {
+        $this->resume = $resume;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGallery()
+    {
+        return $this->gallery;
+    }
+
+    /**
+     * @param mixed $gallery
+     */
+    public function setGallery($gallery)
+    {
+        $this->gallery = $gallery;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+}
